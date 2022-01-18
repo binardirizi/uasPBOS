b632a0c812d7335be2bb2513f2c69ea6e313c5db
Binardi Rizi <90200598+binardirizi@users.noreply.github.com>
2022-01-18 16:59:28 +0700

    No 1

---
+++ uasPBOS.py
@@ -0,0 +1,13 @@
+print ("Jawaban No 1")
+print("MENGHITUNG KELILING DAN LUAS PERSEGI PANJANG")
+print("======================================================================")
+panjang = float(input("\nMasukan Panjang: "))
+lebar = float(input("Masukan Lebar  : "))
+
+keliling = 2*(panjang+lebar)
+luas = panjang*lebar
+
+# mencetak isi variabel
+print("Keliling Persegi Panjang:",keliling)
+print("Luas Persegi Panjang:",luas)
+print("======================================================================")
